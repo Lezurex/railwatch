0ceae1aa2de26cc73986b1743225ffadb5e1b377
Lenny <lenny.angst@easyid.ch>
2021-10-26 16:02:31 +0200

    chore: remov unused imports

--- src/main/java/com/lezurex/railwatch/objects/Journey.java
+++ src/main/java/com/lezurex/railwatch/objects/Journey.java
@@ -1,9 +1,6 @@
 package com.lezurex.railwatch.objects;
 
 import java.util.Arrays;
-import java.util.Date;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
