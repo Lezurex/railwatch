b9b28a365975f8a8be1c5b5674722d9f09e39e29
Draxx7236 <shajnik7@gmail.com>
2021-10-26 14:58:50 +0200

    added Menu ui

---
+++ src/main/java/com/lezurex/railwatch/Menu.java
@@ -0,0 +1,19 @@
+package com.lezurex.railwatch;
+
+public class Menu {
+
+    public void print() {
+
+        System.out.println("------------------------------");
+        System.out.println("----------Railwatch-----------");
+        System.out.println("Type in the Departure Station: ");
+        System.out.println("Type in the Station you're heading to: ");
+        System.out.println("Type in the Time you want to depart: ");
+        System.out.println("------------------------------");
+        System.out.println("Connections");
+        System.out.println("------------------------------");
+        System.out.println("Departure Time" + " ---- " + "Journey Length" + " ---- " + "Platform");
+        System.out.println("//getDepTime" + " ---- " + "//getLength" + " ---- " + "//getPlatform");
+        System.out.println("------------------------------");
+    }
+}
