10143e2e3dae68e07c90555d246b474bf57283c8
Draxx7236 <shajnik7@gmail.com>
2021-11-02 14:06:09 +0100

    added constructor to Menu

--- src/main/java/com/lezurex/railwatch/App.java
+++ src/main/java/com/lezurex/railwatch/App.java
@@ -1,7 +1,18 @@
 package com.lezurex.railwatch;
 
+import com.lezurex.railwatch.objects.Connection;
+import com.lezurex.railwatch.objects.Section;
+import com.lezurex.railwatch.objects.Stop;
+
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        Menu menu = new Menu(sc, new Connection(), new Stop(), new Section());
+        menu.print();
+
     }
 }
--- src/main/java/com/lezurex/railwatch/Menu.java
+++ src/main/java/com/lezurex/railwatch/Menu.java
@@ -4,21 +4,34 @@ import com.lezurex.railwatch.objects.Connection;
 import com.lezurex.railwatch.objects.Section;
 import com.lezurex.railwatch.objects.Stop;
 
+import java.util.Scanner;
+
 public class Menu {
 
-    Connection connection = new Connection();
-    Stop stop = new Stop();
-    Section section = new Section();
+    private Connection connection;
+    private Stop stop;
+    private Section section;
+    private Scanner sc;
+
+    public Menu(Scanner sc, Connection connection, Stop stop, Section section){
+        this.sc = sc;
+        this.connection = connection;
+        this.stop = stop;
+        this.section = section;
+    }
 
     public void print() {
 
         System.out.println("------------------------------");
         System.out.println("----------Railwatch-----------");
         System.out.println("Type in the Departure Station: ");
+        sc.nextLine();
         System.out.println("Type in the Station you're heading to: ");
+        sc.nextLine();
         System.out.println("Type in the Time you want to depart: ");
+        sc.nextLine();
         System.out.println("------------------------------");
-        System.out.println("Connections");
+        System.out.println("         Connections");
         System.out.println("------------------------------");
         System.out.println("Departure Time" + " ---- " + "Journey Length" + " ---- " + "Platform");
         System.out.println(section.getDeparture() + " ---- " + connection.getDuration() + " ---- " + stop.getPlatform());
